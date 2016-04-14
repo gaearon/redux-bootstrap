46be51e02597c87a53aab3f46c94dd87a0f627aa
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-04-14 10:47:36 +0100

    Update configureStore.dev.js

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,21 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import createLogger from "redux-logger";
 import DevTools from "../containers/DevTools";
 
-export default function configureStore(middleware, rootReducer, reducersPath, initialState) {
+export default function configureStore(middleware, rootReducer, initialState) {
     const store = createStore(
         rootReducer,
         initialState,
         compose(
-            applyMiddleware(...[...middleware, createLogger()]),
+            applyMiddleware(...middleware),
             DevTools.instrument()
         )
     );
 
     // Enable Webpack hot module replacement for reducers
     if (module.hot) {
-        module.hot.accept(reducersPath, () => {
+        module.hot.accept("../reducers", () => {
             store.replaceReducer(rootReducer);
         });
     }
